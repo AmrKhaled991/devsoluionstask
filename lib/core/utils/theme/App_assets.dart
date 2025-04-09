17a73dc11b8636ac4ec302a8230812d9e389bbc5
Amr Kaled <amrkhaled033@gmail.com>
2025-04-09 07:34:14 +0200

    handle structure

---
+++ lib/core/utils/theme/App_assets.dart
@@ -0,0 +1,25 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesHeart
+  /// assets/images/heart.svg
+  static const String imagesHeart = "assets/images/heart.svg";
+
+  /// Assets for imagesNotification
+  /// assets/images/notification.svg
+  static const String imagesNotification = "assets/images/notification.svg";
+
+  /// Assets for imagesSearch
+  /// assets/images/search.svg
+  static const String imagesSearch = "assets/images/search.svg";
+
+  /// Assets for imagesShare
+  /// assets/images/share.svg
+  static const String imagesShare = "assets/images/share.svg";
+
+  /// Assets for imagesStare
+  /// assets/images/stare.svg
+  static const String imagesStare = "assets/images/stare.svg";
+}
+
